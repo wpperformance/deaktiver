a10af60051558fa3014db87bb506570df0dbe82a
Patrick Faramaz <ipatate@users.noreply.github.com>
2024-12-14 09:06:26 +0100

    just update version for test

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Deaktiver
  * Plugin URI:        false
  * Description:       Disable native features of WordPress that you don't need.
- * Version:           0.0.2
+ * Version:           0.0.3
  * Requires at least: 6.0
  * Requires PHP:      8.0
  * Author:            Faramaz patrick
